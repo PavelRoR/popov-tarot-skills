a34ee06921ac4a0e63d61c7ac154c3fd7ab5cc93
pavelp21 <pavelvp2182@yandex.ru>
2020-08-05 20:52:28 +0500

    Main page and Promo are typeseted and stylezed

--- _app/_js/scripts.js
+++ _app/_js/scripts.js
@@ -1,8 +1,171 @@
 //@prepros-prepend jquery-2.1.1.min.js
+//@prepros-prepend bootstrap.min.js
+//@prepros-prepend slick.min.js
+//@prepros-prepend popup.js
 
-$(document).ready(function() {
+$(document).ready(function () {
+    $(function () {
+        var check = $('.check', this),
+            email = $('.input-mail', this),
+            message = $('.alert-message', this),
+            button = $('.button-form', this),
+            switch_title = $('.switch-title'),
+            switch_text = $('.switch-text'),
+            currDate = new Date(),
+            startDate = new Array(
+                new Date("June 24 2020 18:30 UTC+3"),
+                new Date("June 25 2020 18:30 UTC+3"),
+                new Date("June 26 2020 18:30 UTC+3")),
+            endDate = new Array(
+                new Date("June 24 2020 21:00 UTC+3"),
+                new Date("June 25 2020 21:00 UTC+3"),
+                new Date("June 26 2020 21:00 UTC+3")),
+            rooms = new Array(
+                'https://pruffme.com/landing/u8937/tmp1592562341',
+                'https://pruffme.com/landing/u8937/tmp1592562341',
+                'https://pruffme.com/landing/u8937/tmp1592562341');
 
+        function roomSwitch() {
+            $('.form-main').addClass('switch-form');
+            switch_title.html('А Вы записались на БЕСПЛАТНЫЙ ВЕБИНАР?<br>ЖДЕМ ТОЛЬКО ВАС!').css('lineHeight', '1.2');
+            switch_text.removeClass('hide').html('<span class="hurryup">Скорее ЗАХОДИТЕ прямо СЕЙЧАС на вебинар!</span>');
+            button.text('Зайти!');
+        }
+        if ((currDate > startDate[0] && currDate < endDate[0]) || (currDate > startDate[1] && currDate < endDate[1]) || (currDate > startDate[2] && currDate < endDate[2])) {
+            roomSwitch();
+        }
+        $(".form").on("submit", function () {
+            var check = $('.check', this),
+                message = $('.alert-message', this),
+                reNone = /.+/,
+                email = $('.input-mail', this),
+                button = $('.button-form', this);
+            if (!email.val().match(reNone)) {
+                message.text('Введите email').slideDown(500);
+                return false;
+            }
+            if (!check.prop("checked")) {
+                check.next().css({
+                    'color': 'red',
+                    'transition': 'all .4s ease'
+                });
+                check.next().children().css({
+                    'color': 'red',
+                    'transition': 'all .4s ease'
+                });
+                message.text('Подтвердите соглашение').slideDown(500);
+                return false;
+            }
+            if (email.val() && check) {
+                if (currDate > startDate[0] && currDate < endDate[0]) {
+                    window.open(rooms[0]);
+                }
+                if (currDate > startDate[1] && currDate < endDate[1]) {
+                    window.open(rooms[1]);
+                }
+                if (currDate > startDate[2] && currDate < endDate[2]) {
+                    window.open(rooms[2]);
+                }
+                button.text('Отправляем...');
+                setTimeout(function () {
+                    button.text('Отправлено!');
+                }, 500);
+                return true
+            }
+        });
+        email.click(function () {
+            // email.css({"borderColor": "rgb(25, 10, 12)",'transition':'all .4s ease'});
+            message.slideUp(500);
+        });
+        check.click(function () {
+            check.next().css({
+                "color": "#000",
+                'transition': 'all .4s ease'
+            });
+            check.next().children().css({
+                "color": "#000",
+                'transition': 'all .4s ease'
+            });
+            message.slideUp(500);
+        });
+    });
+    $('.button-up').magnificPopup({
+        type: 'inline',
+        midClick: true, // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+        mainClass: 'mfp-fade'
+    });
+    $('.revs').slick({
+        autoplay: false,
+        autoplaySpeed: 3000,
+        pauseOnHover: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false,
+        speed: 300,
+        arrows: true,
+        centerPadding: '15px',
+        // adaptiveHeight: true,
+        centerMode: true,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 385,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+        ]
+    });
+    /*Видео-отзывы*/
+    $(".video-wrapper-rev img").click(function () {
+        var a = $(this).parent().attr("data-youtube");
+        $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1&mute=1&modestbranding=1"  allowfullscreen></iframe>')
+    });
+    $('.revs-video').on('swipe', function (event, slick, direction) {
+        $('.video-wrapper-rev iframe').each(function () {
+            // var v =$('.video_rev_img');
+            var l = $(this).parent().attr('data-img');
+            $(this).parent().html('<img src="' + l + '" alt="Видео отзыв">');
+            console.log(l);
+        });
+        $(".video-wrapper-rev img").click(function () {
+            var a = $(this).parent().attr("data-youtube");
+            $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?mute=1&showinfo=0&rel=0&autoplay=1&modestbranding=1"  allowfullscreen></iframe>');
+            console.log(a);
+        });
+    });
+    $('.revs-video .slick-arrow').on('click', function () {
+        $('.video-wrapper-rev iframe').each(function () {
+            // var v =$('.video_rev_img');
+            var l = $(this).parent().attr('data-img');
+            $(this).parent().html('<img src="' + l + '" alt="Видео отзыв">');
+            console.log(l);
+        });
+        $(".video-wrapper-rev img").click(function () {
+            var a = $(this).parent().attr("data-youtube");
+            $(this).parent().html('<iframe src="https://www.youtube.com/embed/' + a + '?mute=1&showinfo=0&rel=0&autoplay=1&modestbranding=1"  allowfullscreen></iframe>');
+            console.log(a);
+        });
+    });
+    $('.revs-text-link').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled: true,
+            preload: [0, 2], // read about this option in next Lazy-loading section
 
+            navigateByImgClick: true,
 
-/*Конец документа*/
+            arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+
+            tPrev: 'Previous (Left arrow key)', // title for left button
+            tNext: 'Next (Right arrow key)', // title for right button
+            tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
+        }
+    });
 });
+    /*Конец документа*/
